e51c4ec7c77c88195fa1acc196b8cd2d150abce2
ArshnoorSingh07 <asingh39_be23@thapar.edu>
2025-07-27 23:39:22 +0530

    models

--- Round 1b/app/download_models.py
+++ Round 1b/app/download_models.py
@@ -1,13 +1,20 @@
 from sentence_transformers import SentenceTransformer
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+import os
 
-# Save MiniLM encoder to app/models
+# === Setup base path ===
+base_path = os.path.dirname(__file__)  # This points to 'Round 1b/app'
+models_path = os.path.join(base_path, "models")
+
+# === Save MiniLM encoder to Round 1b/app/models/all-MiniLM-L6-v2 ===
 encoder = SentenceTransformer("all-MiniLM-L6-v2")
-encoder.save("models/all-MiniLM-L6-v2")
+minilm_path = os.path.join(models_path, "all-MiniLM-L6-v2")
+encoder.save(minilm_path)
 
-# Save Flan-T5-small to app/models
+# === Save Flan-T5-small to Round 1b/app/models/flan-t5-small ===
 tokenizer = AutoTokenizer.from_pretrained("google/flan-t5-small")
 model = AutoModelForSeq2SeqLM.from_pretrained("google/flan-t5-small")
 
-tokenizer.save_pretrained("models/flan-t5-small/tokenizer")
-model.save_pretrained("models/flan-t5-small/model")
+flan_t5_path = os.path.join(models_path, "flan-t5-small")
+tokenizer.save_pretrained(os.path.join(flan_t5_path, "tokenizer"))
+model.save_pretrained(os.path.join(flan_t5_path, "model"))
